e85af921d9c90aabc8b5eabb7d65aaa918dbca28
GlamorousNutlet <glamorousnutlet@gmail.com>
2020-07-24 10:52:51 +0300

    -- End of email_valid func • add to user model confirmation_code • rename in views User to CustomUser • PEP8 fix

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/',
+         TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/',
+         TokenRefreshView.as_view(), name='token_refresh'),
 ]
